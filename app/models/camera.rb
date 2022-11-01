691fe02668fbd1f95054e9443961dc9a20ba393a
Takashi <th2826@columbia.edu>
2022-10-31 23:01:48 -0400

    validations added to Camera model

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -3,4 +3,9 @@ class Camera < ApplicationRecord
   belongs_to :user
 
   has_many :comments
+
+  validates :user_id, presence: true
+  validates :neighborhood_id, presence: true
+  validates :image_url, presence: true
+  validates :description, length: { maximum: 150 }
 end
