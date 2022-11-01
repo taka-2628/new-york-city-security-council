340f49f03ab7ac90747a2704adaf6011fede0a11
Takashi <th2826@columbia.edu>
2022-10-31 21:55:46 -0400

    routes for Camera, Comment, Neighborhood, Type and SocialMediaPlatform created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/hello', to: 'application#hello_world' 
+  get '/hello', to: 'application#hello_world'
+  
+  resources :cameras
+  resources :comments, except: [:index, :show]
+  resources :neighborhoods, only: [:index]
+  resources :social_media_platforms, only: [:index]
 
   get '*path',
       to: 'fallback#index',
