97d443038c231c8ef6ee1378ed0e16353f305366
Takashi <th2826@columbia.edu>
2022-11-01 20:53:39 -0400

    react: LoginForm & SignupForm components created

--- client/src/components/LoginSignup.js
+++ client/src/components/LoginSignup.js
@@ -1,10 +1,14 @@
 import NavBar from "./NavBar";
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
 
 function LoginSignup(){
   return(
-    <div id="login-signup">
-      <p>Signin/Signup</p>
+    <div id="login-signup" className="width-60">
+      <h1>Signin/Signup</h1>
       <NavBar />
+      <LoginForm />
+      <SignupForm />
     </div>
   )
 }
