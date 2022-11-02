010328c5b1d84a2b16529035b5b56538eb7a8540
Takashi <th2826@columbia.edu>
2022-11-02 15:14:53 -0400

    react: GeolocateAddress, Geolocate and Address components created

--- client/src/components/Contribute.js
+++ client/src/components/Contribute.js
@@ -1,11 +1,85 @@
-import NavBar from "./NavBar";
-import CameraSubmitForm from "./CameraSubmitForm";
+import { useState } from "react";
+
+import CloseBtn from "./buttons/CloseBtn";
+import ReturnBtn from "./buttons/ReturnBtn";
+import GeolocateAddress from "./submit-forms/GeolocateAddress";
+
 
 function Contribute(){
+  const [step, setStep] = useState(0);
+
+  function onStepChange(){
+    setStep(step + 1)
+  }
+
+  const [coordinates, setCoordinates] = useState(null);
+  const [formData, setFormData] = useState(
+    {
+      address: "",
+      intersection: "",
+      zipcode: "",
+      borough: "",
+      image_url: "",
+      description: "",
+    }
+  );
+  
+  function handleChange(key, value){
+    setFormData({
+      ...formData, 
+      [key]: value,
+    })
+  }
+
+  function handleSubmit(){
+    const data = { ...formData, geometry: coordinates}
+    console.log(data);
+    
+    /*
+    fetch("/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id: currentUser.id,
+        title,
+        subtitle,
+        description,
+        image,
+        url,
+        github_url: github,
+        genres: selectedGenres,
+        technologies: selectedTechs
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((project) => {
+          setProjects([...projects, project])
+          navigate("/");
+        });
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+    */
+  }
+
+  const conditionalComponent = () => {
+    switch (step) {
+      case 0:
+        return <GeolocateAddress coordinates={coordinates} setCoordinates={setCoordinates} formData={formData} handleChange={handleChange} onStepChange={onStepChange}/>;
+      default: 
+        return <GeolocateAddress coordinates={coordinates} setCoordinates={setCoordinates} formData={formData} handleChange={handleChange} onStepChange={onStepChange}/>;
+    }
+  }
+
   return(
     <div id="contribute" className="grid-container">
-      <NavBar />
-      <CameraSubmitForm />
+      <CloseBtn />
+      <ReturnBtn />
+
+      {conditionalComponent()}
     </div>
   )
 }
