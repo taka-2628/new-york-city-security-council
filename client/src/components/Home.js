27f10929a74bde8b49ada1ff28b321c12902796e
Takashi <th2826@columbia.edu>
2022-11-02 13:41:26 -0400

    react: Home component and its styling updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,18 @@
 import NavFullscreen from "./NavFullscreen";
+import '../stylesheets/Home.css';
 
 function Home(){
   return(
     <div id="home">
       <p>Home</p>
       <NavFullscreen />
+      <div id="dark-filter"></div>
+      <div id="title-animation-cont">
+        <div id="title-animation-wrapper">
+          <h1>New York City Security Council</h1>
+          <h1 className="title-second">New York City Security Council</h1>
+        </div>
+      </div>
     </div>
   )
 }
