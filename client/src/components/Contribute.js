9fdcf65479cf8938d2fe881ab91f75055c675997
Takashi <th2826@columbia.edu>
2022-11-01 20:58:12 -0400

    react: CameraSubmitForm component created

--- client/src/components/Contribute.js
+++ client/src/components/Contribute.js
@@ -1,10 +1,12 @@
 import NavBar from "./NavBar";
+import CameraSubmitForm from "./CameraSubmitForm";
 
 function Contribute(){
   return(
     <div id="contribute">
       <p>Contribute</p>
       <NavBar />
+      <CameraSubmitForm />
     </div>
   )
 }
