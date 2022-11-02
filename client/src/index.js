e47dd8b7bef07e86411e6651a47c85d71fc84260
Takashi <th2826@columbia.edu>
2022-11-02 12:45:15 -0400

    react: UserProvider added in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+import { UserProvider } from './context/user';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
 
