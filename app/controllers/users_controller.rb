b2e8e444489036db0e49052c13485d0d2db467bb
Takashi <th2826@columbia.edu>
2022-11-14 23:54:13 -0500

    include: option neighborhood, cameras added to UsersController #index, #show and #create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,13 @@ class UsersController < ApplicationController
 
   def index 
     users = User.all
-    render json: users, include: ['social_media_platforms']
+    render json: users, include: ['neighborhood', 'cameras', 'social_media_platforms']
   end
 
   def show
     user = User.find_by(id: session[:user_id])
     if user
-      render json: user, status: :ok, include: ['social_media_platforms']
+      render json: user, status: :ok, include: ['neighborhood', 'cameras', 'social_media_platforms']
     else
       render json: { error: "Not authorize" }, status: :unauthorized
     end
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     end
     if user.valid?
       session[:user_id] = user[:id]
-      render json: user, status: :created
+      render json: user, status: :created, include: ['neighborhood', 'cameras', 'social_media_platforms']
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
