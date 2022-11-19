da1114aa5dbe44a85100b7fb79cef154aa0bb267
Takashi <th2826@columbia.edu>
2022-11-18 23:29:39 -0500

    react: SNS logos imported

--- client/src/components/Community.js
+++ client/src/components/Community.js
@@ -3,9 +3,29 @@ import '../stylesheets/Community.css';
 import NavBar from "./NavBar";
 import CloseBtn from "./buttons/CloseBtn";
 
+import logoFacebook from "../assets/sns-Facebook.png";
+import logoInstagram from "../assets/sns-Instagram.png";
+import logoTwitter from "../assets/sns-Twitter.png";
+import logoTikTok from "../assets/sns-TikTok.png";
+import logoSnapchat from "../assets/sns-Snapchat.png";
+import logoPinterest from "../assets/sns-Pinterest.png";
+import logoLinkedIn from "../assets/sns-LinkedIn.png";
+import logoBeReal from "../assets/sns-BeReal.png";
+
 function Community( {users} ){
 
   const userList = users.map((user) => {
+    const logos = {
+      Facebook: logoFacebook,
+      Instagram: logoInstagram,
+      Twitter: logoTwitter,
+      TikTok: logoTikTok,
+      Snapchat: logoSnapchat,
+      Pinterest: logoPinterest,
+      LinkedIn: logoLinkedIn,
+      BeReal: logoBeReal
+    }
+    
     return (
       <div key={user.id} className='user-cont'>
         <p><span>user:</span><strong>{user.username}</strong><span>neighborhood:</span><strong>{user.neighborhood.neighborhood}</strong></p>
