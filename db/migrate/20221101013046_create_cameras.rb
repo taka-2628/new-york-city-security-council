2bd18edca6520b22072e6709a5e8b430a4b110db
Takashi <th2826@columbia.edu>
2022-11-30 18:53:00 -0500

    create_camera migration updated - geometry is replaced with latitude and longitude

--- db/migrate/20221101013046_create_cameras.rb
+++ db/migrate/20221101013046_create_cameras.rb
@@ -4,7 +4,8 @@ class CreateCameras < ActiveRecord::Migration[7.0]
       t.integer :user_id
       t.integer :neighborhood_id
       t.string :image_url
-      t.string :geometry
+      t.string :latitude
+      t.string :longitude
       t.string :address
       t.string :intersection
       t.string :zipcode
