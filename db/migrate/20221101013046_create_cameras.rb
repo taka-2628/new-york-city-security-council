d357b8aa7263f5cddc9d1a0d8b062e26ef512af7
Takashi <th2826@columbia.edu>
2022-11-30 19:05:34 -0500

    create_camera migration updated - data type changed from decimal to float for latitude and longitude

--- db/migrate/20221101013046_create_cameras.rb
+++ db/migrate/20221101013046_create_cameras.rb
@@ -4,8 +4,8 @@ class CreateCameras < ActiveRecord::Migration[7.0]
       t.integer :user_id
       t.integer :neighborhood_id
       t.string :image_url
-      t.decimal :latitude
-      t.decimal :longitude
+      t.float :latitude
+      t.float :longitude
       t.string :address
       t.string :intersection
       t.string :zipcode
