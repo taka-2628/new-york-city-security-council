6c9c42699bae8f85ec7e0674c1829fd7f41a94cf
Takashi <th2826@columbia.edu>
2022-12-08 21:49:30 -0500

    react: SidePanel component and its styling updated

--- client/src/components/map-components/SidePanel.js
+++ client/src/components/map-components/SidePanel.js
@@ -10,43 +10,45 @@ function SidePanel({ isSidePanelOpen, cameraSelected, toggleSidebar, cameras, se
   return(
     <>
       <div className={`side-panel ${isSidePanelOpen && cameraSelected ? 'active' : ''}`}>
-        {
-          cameraSelected
-          ? <div id="camera-info-cont">
-              <div id="image-wrapper">
-                <img src={cameraSelected.image_url}></img>
+        <div className='side-panel-wrapper'>
+          {
+            cameraSelected
+            ? <div id="camera-info-cont">
+                <div id="image-wrapper">
+                  <img src={cameraSelected.image_url}></img>
+                </div>
+                <div id="text-wrapper">
+                  <h4>Camera Info</h4>
+                  <div className="cam-info-text-group">
+                    <><span>Uploaded by {cameraSelected.user.username}</span><br/></>
+                    <><span>{formatedDate}</span><br/></>
+                  </div>
+                  <hr/>
+                  <div className="cam-info-text-group">
+                    { cameraSelected.latitude && cameraSelected.longitude ? <><span>Lat: {cameraSelected.latitude}, Long: {cameraSelected.longitude}</span><br/></> : null}
+                    { cameraSelected.address ? <><span>Address: {cameraSelected.address}</span><br/></> : null }
+                    { cameraSelected.intersection ? <><span>Intersection: {cameraSelected.intersection}</span><br/></> : null }
+                    { cameraSelected.zipcode ? <><span>Zipcode: {cameraSelected.zipcode}</span><br/></> : null }
+                    { cameraSelected.neighborhood.neighborhood ? <><span>Neighborhood: {cameraSelected.neighborhood.neighborhood}</span><br/></> : null }
+                    { cameraSelected.neighborhood.borough ? <><span>Borough: {cameraSelected.neighborhood.borough}</span><br/></> : null }
+                  </div>
+                  <hr/>
+                  <div className="cam-info-text-group">
+                    <span>Type: {cameraSelected.camera_type}</span><br/>
+                    <span>Owner: {cameraSelected.owner}</span><br/>
+                  </div>
+                  <hr/>
+                  <div className="cam-info-text-group">
+                    <span>Description: </span>
+                    <p>{cameraSelected.description}</p>
+                  </div>
+                </div>
               </div>
-              <div id="text-wrapper">
-                <h4>Camera Info</h4>
-                <div className="cam-info-text-group">
-                  <><span>Uploaded by {cameraSelected.user.username}</span><br/></>
-                  <><span>{formatedDate}</span><br/></>
-                </div>
-                <hr/>
-                <div className="cam-info-text-group">
-                  { cameraSelected.latitude && cameraSelected.longitude ? <><span>Lat: {cameraSelected.latitude}, Long: {cameraSelected.longitude}</span><br/></> : null}
-                  { cameraSelected.address ? <><span>Address: {cameraSelected.address}</span><br/></> : null }
-                  { cameraSelected.intersection ? <><span>Intersection: {cameraSelected.intersection}</span><br/></> : null }
-                  { cameraSelected.zipcode ? <><span>Zipcode: {cameraSelected.zipcode}</span><br/></> : null }
-                  { cameraSelected.neighborhood.neighborhood ? <><span>Neighborhood: {cameraSelected.neighborhood.neighborhood}</span><br/></> : null }
-                  { cameraSelected.neighborhood.borough ? <><span>Borough: {cameraSelected.neighborhood.borough}</span><br/></> : null }
-                </div>
-                <hr/>
-                <div className="cam-info-text-group">
-                  <span>Type: {cameraSelected.camera_type}</span><br/>
-                  <span>Owner: {cameraSelected.owner}</span><br/>
-                </div>
-                <hr/>
-                <div className="cam-info-text-group">
-                  <span>Description: </span>
-                  <p>{cameraSelected.description}</p>
-                </div>
-              </div>
-            </div>
-          : null
-        }
-        <hr/>
-        {cameraSelected ? <CommentSection comments={cameraSelected.comments} cameraSelected={cameraSelected} cameras={cameras} setCameras={setCameras}/> : null}
+            : null
+          }
+          <div id="hr-wrapper"><hr/></div>
+          {cameraSelected ? <CommentSection comments={cameraSelected.comments} cameraSelected={cameraSelected} cameras={cameras} setCameras={setCameras}/> : null}
+        </div>
       </div>
       <div className={`side-panel-overlay ${isSidePanelOpen == true ? 'active' : ''}`} onClick={toggleSidebar}></div>
     </>
