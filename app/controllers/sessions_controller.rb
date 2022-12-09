b83f8c44078d0e7ea7286ae0de983e645f7dd8f0
Takashi <th2826@columbia.edu>
2022-12-08 22:48:53 -0500

    authorize method added in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :authorize
+  skip_before_action :authorize, only: [:create]
 
   def create
     user = User.find_by(username: params[:username])
@@ -13,6 +15,12 @@ class SessionsController < ApplicationController
   def destroy
     session.delete :user_id
     head :no_content
+  end
+
+  private
+
+  def authorize
+    return render json: { error: "Not logged in" }, status: :unauthorized unless session.include? :user_id
   end
   
 end
