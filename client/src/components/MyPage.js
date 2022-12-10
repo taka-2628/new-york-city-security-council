413baf8459bac9c4c6609bdf05a50a7fe4c9adb1
Takashi <th2826@columbia.edu>
2022-12-10 15:58:34 -0500

    react: MyPage component updated

--- client/src/components/MyPage.js
+++ client/src/components/MyPage.js
@@ -9,9 +9,8 @@ import CloseBtn from "./buttons/CloseBtn";
 function MyPage(){
   const { user } = useContext(UserContext);
 
-  const [userData, setUserData] = useState(user);
-  console.log(userData);
-  
+  const [userData, setUserData] = useState(user);  
+  console.log(userData)
   
   return(
     <div id="my-page" className="grid-container">
@@ -24,22 +23,63 @@ function MyPage(){
           <form>
             { 
               Object.keys(userData).map((key, index) => {
-                if (key !== "social_media_platforms" && key !== "id"){
+                if (key !== "social_media_platforms" && key !== "id" && key !== "cameras" && key !== "neighborhood"){
                   return (
                     <div key={index}>
                       <label>{key.replace(/_+/g, ' ')}</label>
-                      <input value={ userData[key] }></input>
+                      <input value={userData[key]}></input>
                     </div>
                   );
-                } else {
-                  console.log(userData[key])
+                } else if(key === "neighborhood") {
+                  return (
+                    <div key={index}>
+                      <label>{key.replace(/_+/g, ' ')}</label>
+                      <input value={userData[key]["neighborhood"]}></input>
+                    </div>
+                  );
                 }
-                
               }) 
             }
           </form>
         </div>
         <hr/>
+        <div id="my-cameras">
+          <h3>My Cameras</h3>
+          {
+            userData.cameras.map((camera) => {
+              const date = camera ? new Date(camera.created_at) : null // formated_Date - SDK returned date
+              const formatedDate = camera ? (`${date.getFullYear()}-${date.getMonth() +1 }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`) : null;
+              return(
+                <div key={camera.id} className="camera-card">
+                  <img src={camera.image_url}></img>
+                  <div className="text-wrapper">
+                  <h4>Camera Info</h4>
+                  <div className="cam-info-text-group">
+                    <><span>{formatedDate}</span><br/></>
+                  </div>
+                  <hr/>
+                  <div className="cam-info-text-group">
+                    { camera.latitude && camera.longitude ? <><span>Lat: {camera.latitude}, Long: {camera.longitude}</span><br/></> : null}
+                    { camera.address ? <><span>Address: {camera.address}</span><br/></> : null }
+                    { camera.intersection ? <><span>Intersection: {camera.intersection}</span><br/></> : null }
+                    { camera.zipcode ? <><span>Zipcode: {camera.zipcode}</span><br/></> : null }
+                  </div>
+                  <hr/>
+                  <div className="cam-info-text-group">
+                    <span>Type: {camera.camera_type}</span><br/>
+                    <span>Owner: {camera.owner}</span><br/>
+                  </div>
+                  <hr/>
+                  <div className="cam-info-text-group">
+                    <span>Description: </span>
+                    <p>{camera.description}</p>
+                  </div>
+                </div>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     </div>
   )
