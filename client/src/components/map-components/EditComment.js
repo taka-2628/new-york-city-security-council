2f61cf84dd478636b4347795638b767a18e800c4
Takashi <th2826@columbia.edu>
2022-12-12 21:28:51 -0500

    react: EditComment component updated

--- client/src/components/map-components/EditComment.js
+++ client/src/components/map-components/EditComment.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function EditComment( { comment, handleEdit }){
   const [commentBody, setCommentBody] = useState(comment.body);
+  const [errors, setErrors] = useState("");
   
   function handleEditFormSubmit(e, id){
     e.preventDefault();
@@ -15,21 +16,37 @@ function EditComment( { comment, handleEdit }){
         body: commentBody
       }),
     })
-    .then((r) => r.json())
-    .then((updatedComment) => handleEdit(updatedComment));
+    .then((r) => {
+      if (r.ok) {
+        r.json().then(updatedComment => handleEdit(updatedComment));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
   }
   
   return (
-    <form className="edit-message" onSubmit={(e) => handleEditFormSubmit(e, comment.id)}>
-      <textarea
-        type="text"
-        name="body"
-        autoComplete="off"
-        value={commentBody}
-        onChange={(e) => setCommentBody(e.target.value)}
-      />
-      <input type="submit" value="Save" />
-    </form>
+    <>
+      <form className="edit-message" onSubmit={(e) => handleEditFormSubmit(e, comment.id)}>
+        <textarea
+          type="text"
+          name="body"
+          autoComplete="off"
+          value={commentBody}
+          onChange={(e) => setCommentBody(e.target.value)}
+        />
+        <input type="submit" value="Save" />
+      </form>
+      <div className="error-div">
+        {
+          errors
+          ? errors.map((err) => (
+            <p key={err} >{err}</p>
+          )) 
+          : null
+        }
+      </div>
+  </>
   );
 }
 
